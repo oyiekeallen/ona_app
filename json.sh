c9e0a47ea1191ab6c3432343c23b2faf3d646c06
Allen Oyieke <oyiekeallen@gmail.com>
2019-07-10 16:27:24 +0300

    update
    added changelog

--- json.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-## Prettier
-function jsonval {
-    echo $1  | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); print "====> JSON Output: \n ";for (i=1; i<=n; i++) print "\t" a[i] }' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g'
-}
-jsonval $1
